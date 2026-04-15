d9c0e78e386ed3f04bde5752b63595698ef955a3
Natog <renato_alberto_neto@hotmail.com>
2026-04-15 16:28:14 -0300

    ocultar valores das transações recentes

--- Angular/PersonalFinance/src/app/pages/home/components/transactions-card/transactions-card.component.ts
+++ Angular/PersonalFinance/src/app/pages/home/components/transactions-card/transactions-card.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
 import { TransactionService } from '../../services/transaction.service';
 import { Transaction } from '../../models/transaction.model';
+import { DashboardService } from '../../services/dashboard.service';
 
 @Component({
   selector: 'app-transactions-card',
@@ -12,7 +13,9 @@ import { Transaction } from '../../models/transaction.model';
 })
 export class TransactionsCardComponent {
   private readonly transactionService = inject(TransactionService);
+  private readonly dashboardService = inject(DashboardService);
   readonly transactions = this.transactionService.transactions;
+  readonly isBalanceHidden = this.dashboardService.isBalanceHidden;
 
   trackById(_: number, item: Transaction): string {
     return item.id;
